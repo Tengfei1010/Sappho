7b8ec101dd354b212f1ffed769e5bf6f99700087
t <tutengfei@126.com>
2014-08-22 18:04:19 +0800

    Add MongoDB config items

--- application/config/config.php
+++ application/config/config.php
@@ -358,5 +358,47 @@ $config['rewrite_short_tags'] = FALSE;
 $config['proxy_ips'] = '';
 
 
+/*
+|--------------------------------------------------------------------------
+|Sappho config MonoDB
+|--------------------------------------------------------------------------
+*/
+// ::CONFIG/mongo_conn/string:: MongoDB Connection Parameters
+// By default you will connect to whatever server is specified in your php.ini configuration, usually mongodb://localhost:27017
+// Format: mongodb://[username:password@]hostname_or_ip[:port][/database]
+// Skip the [username:password@] part if your server doesn't do authentication
+// Skip the [:port] part to use the default port (27017)
+// Skip the [/database] part if your user is a global admin, and  does not belong to the specific database
+
+$config['mongo_conn']	= 'mongodb://root:root@localhost:27017';
+
+
+// ::CONFIG/mongo_db/string:: Database to connect to
+$config['mongo_db'] = 'TestMongoDB';
+/*
+// ::CONFIG/mongo_db/string:: Username to connect to
+$config['username'] = 'database';
+
+// ::CONFIG/mongo_db/string:: Password to connect to
+$config['password'] = 'password';
+
+// ::CONFIG/mongo_db/string:: Host to connect to
+$config['Host'] = '127.0.0.1';
+
+// ::CONFIG/mongo_db/string:: Database to connect to
+$config['port'] = '27017';
+*/
+
+// ::CONFIG/mongo_friendly/string:: Friendly database name
+// Once the connection is initialized and the database selected, the underlying MongoDB object will be attached to the Mdb object, and made available through the controller ($this->mdb).
+// Normally, we name the attached object the same name as the database, so if you're connected to the 'openpanels' database, you can access the MongoDB object using $this->mdb->openpanels, but you can override that name below, this is useful if your db name is long, complicated or would conflict with other variable names.
+$config['mongo_friendly'] = '';
+
+// ::CONFIG/mongo_persist/string:: Persistent string
+// According to PHP manual: Persistent connections need an identifier string to uniquely identify them. For a persistent connection to be used, the hostname, port, persist string, and username and password (if given) must match an existing persistent connection. Otherwise, a new connection will be created with this identifying information.
+$config['mongo_persist'] = 'cimongodb';
+
+
+
 /* End of file config.php */
 /* Location: ./application/config/config.php */
